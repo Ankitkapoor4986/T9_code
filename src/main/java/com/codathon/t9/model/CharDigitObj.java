e28f6229ea6dbead27bb13d4b3e3322dc081f1ec
Ankit Kapoor <akapoor@xebia.com>
2016-07-23 13:18:30 +0530

    Initial commit

---
+++ src/main/java/com/codathon/t9/model/CharDigitObj.java
@@ -0,0 +1,25 @@
+package com.codathon.t9.model;
+
+/**
+ * Created by ankit on 23/7/16.
+ */
+public class CharDigitObj {
+    private int digit;
+    private char characterReq;
+
+    public int getDigit() {
+        return digit;
+    }
+
+    public void setDigit(int digit) {
+        this.digit = digit;
+    }
+
+    public char getCharacterReq() {
+        return characterReq;
+    }
+
+    public void setCharacterReq(char characterReq) {
+        this.characterReq = characterReq;
+    }
+}
